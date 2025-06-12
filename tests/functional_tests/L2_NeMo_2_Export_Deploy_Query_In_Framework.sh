f208a72dcd5305c13a69abbfd2285ab85f9f6581
oliver könig <okoenig@nvidia.com>
2025-06-12 07:03:42 +0200

    ci: Use more coverage commands (#114)
    * ci: Use more coverage commands
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * f
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * test
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * fix
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    ---------
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L2_NeMo_2_Export_Deploy_Query_In_Framework.sh
+++ tests/functional_tests/L2_NeMo_2_Export_Deploy_Query_In_Framework.sh
@@ -11,17 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-python tests/functional_tests/utils/test_hf_import.py \
+coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode tests/functional_tests/utils/test_hf_import.py \
   --hf_model meta-llama/Llama-3.2-1B \
   --output_path /tmp/nemo2_ckpt \
   --config Llama32Config1B
 
-torchrun --nproc_per_node=2 scripts/deploy/nlp/deploy_inframework_triton.py \
+coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode -m torch.distributed.run --nproc_per_node=2 scripts/deploy/nlp/deploy_inframework_triton.py \
   --nemo_checkpoint /tmp/nemo2_ckpt \
   --triton_model_name llama \
   --tensor_parallelism_size 2 &
 
-coverage run -a --data-file=/workspace/.coverage --source=/workspace scripts/deploy/nlp/query_inframework.py \
+coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode scripts/deploy/nlp/query_inframework.py \
   --model_name llama \
   --prompt "What is the color of a banana?" \
   --max_output_len 20
+coverage combine
