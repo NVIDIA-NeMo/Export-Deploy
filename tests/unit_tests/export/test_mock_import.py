2bf946e1537b8119f15c52c20d985e9b09814413
Jan Lasek <janek.lasek@gmail.com>
2025-06-17 21:21:08 +0200

    Fix CICD pipeline (#141)
    * Drop no longer used use_embedding_sharing param
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    * Restore some unit tests
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    * Fix and test for run_nemo_deploy.py
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    * Skip F401 check here
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    * Try mounting test data for unit tests
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    * Revert changes to test_tensorrt_llm_export.py
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>
    
    ---------
    
    Signed-off-by: Jan Lasek <jlasek@nvidia.com>

--- tests/unit_tests/export/test_mock_import.py
+++ tests/unit_tests/export/test_mock_import.py
@@ -27,12 +27,10 @@ def test_mock_import_existing_module():
         assert math is math_org
 
 
-@pytest.mark.skip
-@pytest.mark.pleasefixme
 def test_mock_import_non_existing_module():
     """Test mocking a non-existing module."""
     with _mock_import("non.existing.module"):
-        pass
+        import non.existing.module  # noqa: F401
 
     with pytest.raises(ModuleNotFoundError):
-        pass
+        import non.existing.module  # noqa: F401
