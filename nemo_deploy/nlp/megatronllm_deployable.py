408cda8ed991c3455e6bcfe0143d57eebb632b7c
oliver könig <okoenig@nvidia.com>
2025-06-18 19:06:25 +0200

    build: Upgrade Mcore (#142)
    * upgrade mcore
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * update
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * Add return_prompt_top_n_logprobs
    
    Signed-off-by: Abhishree <abhishreetm@gmail.com>
    
    ---------
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    Signed-off-by: Abhishree <abhishreetm@gmail.com>
    Co-authored-by: Abhishree <abhishreetm@gmail.com>

--- nemo_deploy/nlp/megatronllm_deployable.py
+++ nemo_deploy/nlp/megatronllm_deployable.py
@@ -218,8 +218,8 @@ class MegatronLLMDeployableNemo2(ITritonDeployable):
 
                 inference_params = CommonInferenceParams(
                     temperature=temperature,
-                    top_k=top_k,
-                    top_p=top_p,
+                    top_k=int(top_k),
+                    top_p=float(top_p),
                     num_tokens_to_generate=num_tokens_to_generate,
                     return_log_probs=log_probs,
                 )
@@ -410,6 +410,7 @@ class MegatronLLMDeployableNemo2(ITritonDeployable):
                     src=0,
                 )
         # cast top_k,top_p to native int, float since typecheck assert statements added in MCore0.13 error otherwise
+        # return_prompt_top_n_logprobs returns top_logprobs for prompt tokens too when top_logprobs>0.
         inference_params = CommonInferenceParams(
             temperature=temperature,
             top_k=int(top_k),
@@ -417,6 +418,7 @@ class MegatronLLMDeployableNemo2(ITritonDeployable):
             num_tokens_to_generate=num_tokens_to_generate,
             return_log_probs=log_probs,
             top_n_logprobs=top_logprobs,
+            return_prompt_top_n_logprobs=bool(top_logprobs),
         )
 
         results = self.generate(prompts, inference_params)
