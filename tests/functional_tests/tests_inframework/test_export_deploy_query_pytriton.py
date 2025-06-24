24c0b7d8b3879a620107626fcf3f93a785141db5
oliver könig <okoenig@nvidia.com>
2025-06-24 13:14:39 +0000

    refactor export-deploy-query-inframework
    Signed-off-by: oliver könig <okoenig@nvidia.com>

---
+++ tests/functional_tests/tests_inframework/test_export_deploy_query_pytriton.py
@@ -0,0 +1,78 @@
+import logging
+import shutil
+import signal
+import subprocess
+import tempfile
+
+from scripts.deploy.nlp.query_inframework import query_llm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TestDeployPyTriton:
+    @classmethod
+    def setup_class(cls):
+        # Create output directories
+        cls.testdir = tempfile.mkdtemp()
+        logger.info(f"Test directory: {cls.testdir}")
+
+        # HF to NeMo2
+        subprocess.run(
+            [
+                "coverage",
+                "run",
+                "--data-file=/workspace/.coverage",
+                "--source=/workspace/",
+                "--parallel-mode",
+                "tests/functional_tests/utils/test_hf_import.py",
+                "--hf_model",
+                "meta-llama/Llama-3.2-1B",
+                "--output_path",
+                f"{cls.testdir}/nemo2_ckpt",
+                "--config",
+                "Llama32Config1B",
+            ],
+            check=True,
+        )
+
+    @classmethod
+    def teardown_class(cls):
+        logger.info(f"Removing test directory: {cls.testdir}")
+        shutil.rmtree(cls.testdir)
+
+    def test_deploy_pytriton(self):
+        # Run deployment
+        deploy_proc = subprocess.Popen(
+            [
+                "torchrun",
+                "--nproc_per_node=2",
+                "--no-python",
+                "coverage",
+                "run",
+                "--data-file=/workspace/.coverage",
+                "--source=/workspace/",
+                "--parallel-mode",
+                "scripts/deploy/nlp/deploy_inframework_triton.py",
+                "--nemo_checkpoint",
+                f"{self.testdir}/nemo2_ckpt",
+                "--triton_model_name",
+                "llama",
+                "--tensor_parallelism_size",
+                str(2),
+            ]
+        )
+
+        outputs = query_llm(
+            url="0.0.0.0",
+            model_name="llama",
+            prompts=["What is the color of a banana?"],
+            max_output_len=20,
+        )
+
+        print(outputs)
+
+        # Check if deployment was successful
+        assert len(outputs) != 0, "Prediction empty"
+
+        deploy_proc.send_signal(signal.SIGINT)
