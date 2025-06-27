0e22e052c24ab615d725f3e3d29c9a59902b2bc2
oliver könig <okoenig@nvidia.com>
2025-06-27 16:31:49 +0000

    update
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L2_Launch_InFramework.sh
+++ tests/functional_tests/L2_Launch_InFramework.sh
@@ -15,20 +15,6 @@
 #!/bin/bash
 set -xeuo pipefail # Exit immediately if a command exits with a non-zero status
 
-<<<<<<<< HEAD:tests/functional_tests/L2_NeMo_2_VLLM_VISION.sh
-export TRANSFORMERS_OFFLINE=1
-export HF_HOME=/home/TestData/vlm/vision/hf/
-
-coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode tests/functional_tests/utils/test_llava_next_InternVIT.py \
-  --devices=1 \
-  --max-steps=5
-
-coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode scripts/vlm/import_hf.py --input_name_or_path="OpenGVLab/InternViT-300M-448px-V2_5"
-
-coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode scripts/vlm/import_hf.py --input_name_or_path="openai/clip-vit-large-patch14"
-
-coverage run --data-file=/workspace/.coverage --source=/workspace --parallel-mode scripts/vlm/import_hf.py --input_name_or_path="google/siglip-base-patch16-224"
-========
 export CUDA_VISIBLE_DEVICES="0,1"
 
 coverage run \
@@ -39,5 +25,4 @@ coverage run \
     -o log_cli=true \
     -o log_cli_level=INFO \
     -vs -m "not pleasefixme" --tb=short tests/functional_tests/tests_inframework
->>>>>>>> d66207772 (ci: Refactor tests (#157)):tests/functional_tests/L2_Launch_InFramework.sh
 coverage combine
