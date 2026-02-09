39a135a589738634e9c885bca78b1eedb084b07a
Charlie Truong <chtruong@nvidia.com>
2026-02-09 01:44:21 +0000

    Revert "Update create_mcore_engine to not raise exception if model format not nemo"
    This reverts commit 445bf8252083aeace92597b8c32f2574bb85c3ed.
    
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- nemo_deploy/llm/inference/inference_base.py
+++ nemo_deploy/llm/inference/inference_base.py
@@ -478,7 +478,7 @@ def create_mcore_engine(
             - GPTInferenceWrapper: Inference-wrapped model
             - Union[MCoreTokenizerWrappper, MegatronTokenizer]: Tokenizer instance
     """
-    if not HAVE_NEMO and model_format == "nemo":
+    if not HAVE_NEMO:
         raise UnavailableError(MISSING_NEMO_MSG)
 
     # Default to 1 for any parallelism dimension that's None
